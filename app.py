16cb5634df6268df392919894aed094f65ff5389
TomekGal <75546362+TomekGal@users.noreply.github.com>
2026-06-17 20:18:12 +0200

    result.points

--- app.py
+++ app.py
@@ -169,7 +169,7 @@ def search_picture():
            
 # show results of searching 
                         
-            for point in result:
+            for point in result.points:
                     score = point.score        # Pobranie wartości score (float)
                     payload = point.payload    # Pobranie powiązanych metadanych (słownik)
                     point_id = point.id        # Pobranie ID punktu
